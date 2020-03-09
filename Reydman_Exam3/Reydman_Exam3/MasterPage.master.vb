5544babcaf2583e28ed013e9b092157df9db4fb7
Arkadiy <arkee333@gmail.com>
2020-03-09 19:05:25 -0400

    CapstoneV_0.1-File Structure / Start

---
+++ Reydman_Exam3/Reydman_Exam3/MasterPage.master.vb
@@ -0,0 +1,10 @@
+﻿
+Partial Class MasterPage
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Unnamed_LoggingOut(sender As Object, e As LoginCancelEventArgs)
+        Context.GetOwinContext().Authentication.SignOut()
+    End Sub
+
+End Class
+
